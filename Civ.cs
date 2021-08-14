ba730dd0f337e45310660ac99d6896143ad19e7d
CatThingy <andyc.andychen@gmail.com>
2021-08-13 22:09:43 -0700

    Added rudimentary stat-changing ability

--- Civ.cs
+++ Civ.cs
@@ -6,9 +6,26 @@ namespace CivSim
     {
         public string Name { get; }
 
+        public int Points { get; set; }
+        public int Respec { get; set; }
+
+        public int Offence { get; set; }
+        public int Defence { get; set; }
+        public int Research { get; set; }
+        public int Education { get; set; }
+        public int Healthcare { get; set; }
+
         public Civ(string name)
         {
             Name = name;
+            Offence = 0;
+            Defence = 0;
+            Research = 0;
+            Education = 0;
+            Healthcare = 0;
+
+            Points = 20;
+            Respec = 10;
         }
     }
 }
--- commands/CivManagementModule.cs
+++ commands/CivManagementModule.cs
@@ -28,6 +28,18 @@ namespace CivSim
             return CivManager.Civs.ContainsKey(userHash);
         }
 
+        // Levels 0-24: costs 1 point
+        // Levels 25-49: costs 2 points
+        // Levels 50-74: costs 3 points
+        // etc.
+        int computePointDifference(int from, int to)
+        {
+            int l1 = (int)Math.Floor(Math.Abs(from) / 25d);
+            int l2 = (int)Math.Floor(Math.Abs(to) / 25d);
+
+            return Math.Sign(to) * (((l2 * (l2 + 1)) / 2) * 25 + (Math.Abs(to) % 25) * (l2 + 1)) - Math.Sign(from) * (((l1 * (l1 + 1)) / 2) * 25 + (Math.Abs(from) % 25) * (l1 + 1));
+        }
+
         [Command("create"), Aliases("register")]
         public async Task CreateCiv(CommandContext context, string name)
         {
@@ -39,7 +51,7 @@ namespace CivSim
             }
             Civ newCiv = new Civ(name);
             CivManager.Civs.Add(userHash, newCiv);
-            
+
             await CivManager.SaveCivs();
             await context.RespondAsync($"{name} has been registered as a nation with an ID of {userHash}");
         }
@@ -56,5 +68,148 @@ namespace CivSim
             await context.RespondAsync("You must specify a name for your nation.");
         }
 
+        [Command("stats")]
+        public async Task ChangeStats(CommandContext context, string stat, int change)
+        {
+            string userHash = GetUserHash(((context.Member as SnowflakeObject ?? context.Guild as SnowflakeObject).Id));
+            if (!UserExists(userHash))
+            {
+                await context.RespondAsync("You haven't registered yet.");
+                return;
+            }
+
+            Civ userCiv = CivManager.Civs[userHash];
+            if (change == 0)
+            {
+                await context.RespondAsync("Congrats, you changed a stat by 0.");
+                return;
+            }
+
+            int p;
+            switch (stat.ToLower())
+            {
+                case "offence":
+                case "offense":
+                    p = computePointDifference(userCiv.Offence, userCiv.Offence + change);
+                    if (change > 0)
+                    {
+                        if (p <= userCiv.Points)
+                        {
+                            userCiv.Offence += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    else
+                    {
+                        if (-p <= userCiv.Respec)
+                        {
+                            userCiv.Offence += p;
+                            userCiv.Respec += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    break;
+
+                case "defence":
+                case "defense":
+                    p = computePointDifference(userCiv.Defence, userCiv.Defence + change);
+                    if (change > 0)
+                    {
+                        if (p <= userCiv.Points)
+                        {
+                            userCiv.Defence += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    else
+                    {
+                        if (-p <= userCiv.Respec)
+                        {
+                            userCiv.Defence += p;
+                            userCiv.Respec += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    break;
+                case "research":
+                    p = computePointDifference(userCiv.Research, userCiv.Research + change);
+                    if (change > 0)
+                    {
+                        if (p <= userCiv.Points)
+                        {
+                            userCiv.Research += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    else
+                    {
+                        if (-p <= userCiv.Respec)
+                        {
+                            userCiv.Research += p;
+                            userCiv.Respec += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    break;
+                case "education":
+                    p = computePointDifference(userCiv.Education, userCiv.Education + change);
+                    if (change > 0)
+                    {
+                        if (p <= userCiv.Points)
+                        {
+                            userCiv.Education += p;
+                            userCiv.Points += p;
+                        }
+                    }
+                    else
+                    {
+                        if (-p <= userCiv.Respec)
+                        {
+                            userCiv.Education += p;
+                            userCiv.Respec += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    break;
+                case "healthcare":
+                    p = computePointDifference(userCiv.Healthcare, userCiv.Healthcare + change);
+                    if (change > 0)
+                    {
+                        if (p <= userCiv.Points)
+                        {
+                            userCiv.Healthcare += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    else
+                    {
+                        if (-p <= userCiv.Respec)
+                        {
+                            userCiv.Healthcare += p;
+                            userCiv.Respec += p;
+                            userCiv.Points -= p;
+                        }
+                    }
+                    break;
+                default:
+                    await context.RespondAsync("That's not a recognized stat.");
+                    return;
+            }
+        }
+
+        [Command("stats")]
+        public async Task ChangeStats(CommandContext context)
+        {
+            string userHash = GetUserHash(((context.Member as SnowflakeObject ?? context.Guild as SnowflakeObject).Id));
+            if (!UserExists(userHash))
+            {
+                await context.RespondAsync("You haven't registered yet.");
+                return;
+            }
+
+            Civ userCiv = CivManager.Civs[userHash];
+
+            await context.RespondAsync($"O:{userCiv.Offence}\nD:{userCiv.Defence}\nR:{userCiv.Research}\nE:{userCiv.Education}\nH:{userCiv.Healthcare}");
+        }
     }
 }
